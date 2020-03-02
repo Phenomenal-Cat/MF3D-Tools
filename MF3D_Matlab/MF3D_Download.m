317945ce5aaf74fd6ddab9473bae3a4fabf5f996
MonkeyGone2Heaven <aidanmurphy1@hotmail.co.uk>
2020-03-02 14:14:31 -0500

    Updated file download & reorganize

--- MF3D_Matlab/MF3D_Download.m
+++ MF3D_Matlab/MF3D_Download.m
@@ -12,8 +12,9 @@
 
 Destination     = uigetdir(cd, 'Select directory to create MF3D R1 in');
 MF3D_root       = fullfile(Destination, 'MF3D_R1');
-success         = mkdir(MF3D_root);
-
+if ~isfolder(MF3D_root)
+	success     = mkdir(MF3D_root);
+end
 Selection       = questdlg('Which subsets of MF3D R1 do you want to download?', ...
                          'Select subsets', ...
                          'Expression', 'Identity', 'Both', 'Both');
@@ -21,23 +22,28 @@ Selection       = questdlg('Which subsets of MF3D R1 do you want to download?', 
 %============= Download expressions
 if any(strcmpi(Selection, {'Expression', 'Both'}))
     MF3D_ExpDir     = fullfile(MF3D_root, 'MF3D_Expressions');
-    success         = mkdir(MF3D_ExpDir);
-    fprintf('Downloading MF3D R1 Expression stimulus set (26.75 GB) to %s...\n', MF3D_ExpDir);
-    ExpressionFiles = {'Summary.csv','Coo','Fear','Neutral','Threat','Tongue','Yawn','Labels'};
-    ExpressionURLs  = {'https://ndownloader.figshare.com/files/15330218',...
-                       'https://ndownloader.figshare.com/files/15330272',...
+    if ~isfolder(MF3D_ExpDir)
+        success     = mkdir(MF3D_ExpDir);
+    end
+    fprintf('Downloading MF3D R1 Expression stimulus set (10.97 GB) to %s...\n', MF3D_ExpDir);
+    outfilename     = websave(fullfile(MF3D_ExpDir, 'MF3D_ExpressionSet_Summary.csv'), 'https://ndownloader.figshare.com/files/15330218');
+    ExpressionFiles = {'Coo','Fear','Neutral','Threat','Tongue','Yawn','Labels'};
+    ExpressionURLs  = {'https://ndownloader.figshare.com/files/15330272',...
                        'https://ndownloader.figshare.com/files/15330389',...
                        'https://ndownloader.figshare.com/files/15330638',...
                        'https://ndownloader.figshare.com/files/15330641',...
                        'https://ndownloader.figshare.com/files/15330647',...
                        'https://ndownloader.figshare.com/files/15330698',...
                        'https://ndownloader.figshare.com/files/15330785'};
-
     wbh = waitbar(0);
     for exp = 1:numel(ExpressionURLs)
-        String          = fprintf('Downloading and unzipping expression ''%s'' (%d / %d)...', ExpressionFiles{exp}, exp, numel(ExpressionURLs));
-        waitbar(exp/ numel(ExpressionURLs), wbh, String);
-        ExpFilenames{exp} = unzip(ExpressionURLs{exp}, MF3D_ExpDir);
+        if ~isfolder(fullfile(MF3D_ExpDir, ExpressionFiles{exp}))
+            String          = sprintf('Downloading and unzipping expression ''%s'' (%d / %d)...', ExpressionFiles{exp}, exp, numel(ExpressionURLs));
+            waitbar(exp/ numel(ExpressionURLs), wbh, String);
+            ExpFilenames{exp} = unzip(ExpressionURLs{exp}, MF3D_ExpDir);
+        else
+            fprintf('Folder ''%s'' already exists. Skipping!\n', fullfile(MF3D_ExpDir, ExpressionFiles{exp}));
+        end
     end
     fprintf('MF3D R1 Expression stimulus set download complete!\n')
 end
@@ -45,11 +51,13 @@ end
 %============= Download identities
 if ny(strcmpi(Selection, {'Identity', 'Both'}))
     MF3D_IdDir     = fullfile(MF3D_root, 'MF3D_Identities');
-    success         = mkdir(MF3D_IdDir);
+    if ~isfolder(MF3D_IdDir)
+        success  	= mkdir(MF3D_IdDir);
+    end
     fprintf('Downloading MF3D R1 Identity stimulus set (26.75 GB) to %s...\n', MF3D_IdDir);
-    IdentityFiles = {'Summary.csv','SD-4','SD-3','SD-2','SD-1','SD0','SD1','SD2','SD3','SD4','Labels'};
-    IdentityURLs  = {  'https://ndownloader.figshare.com/files/15330716',...
-                       'https://ndownloader.figshare.com/files/15371396',...
+    IdentityFiles = {'SD-4','SD-3','SD-2','SD-1','SD0','SD1','SD2','SD3','SD4','Labels'};
+    outfilename   = websave(fullfile(MF3D_IdDir, 'MF3D_IdentitySet_Summary.csv'), 'https://ndownloader.figshare.com/files/15330716');
+    IdentityURLs  = {  'https://ndownloader.figshare.com/files/15371396',...
                        'https://ndownloader.figshare.com/files/15371444',...
                        'https://ndownloader.figshare.com/files/15371495',...
                        'https://ndownloader.figshare.com/files/15371522',...
@@ -60,9 +68,14 @@ if ny(strcmpi(Selection, {'Identity', 'Both'}))
                        'https://ndownloader.figshare.com/files/15371753',...
                        'https://ndownloader.figshare.com/files/15371270'};
     for id = 1:numel(IdentityURLs)
-        String          = fprintf('Downloading and unzipping identity ''%s'' (%d / %d)...', IdentityFiles{id}, id, numel(IdentityURLs));
-        waitbar(id/numel(IdentityURLs), wbh, String);
-        IdFilenames{id} = unzip(IdentityURLs{id}, MF3D_IdDir);
+        if ~isfolder(fullfile(MF3D_IdDir, IdentityFiles{id}))
+            String          = sprintf('Downloading and unzipping identity ''%s'' (%d / %d)...', IdentityFiles{id}, id, numel(IdentityURLs));
+            waitbar(id/numel(IdentityURLs), wbh, String);
+            IdFilenames{id} = unzip(IdentityURLs{id}, MF3D_IdDir);
+        else
+            fprintf('Folder ''%s'' already exists. Skipping!\n', fullfile(MF3D_IdDir, IdentityFiles{id}));
+        end
     end
     fprintf('MF3D R1 Identity stimulus set download complete!\n')
 end
+delete(wbh);
--- MF3D_Matlab/MF3D_Reorganize.m
+++ MF3D_Matlab/MF3D_Reorganize.m
@@ -1,17 +1,57 @@
+function MF3D_Reorganize(MF3D_root)
 
 %========================== MF3D_Reorganize.m =============================
-% Ths Matlab function 
-%
-% See https://github.com/MonkeyGone2Heaven/MF3D-Tools/wiki/MF3D-Stimulus-Editor
-% for further instructions.
+% Ths Matlab function reorganizes the downloaded image files in the MF3D R1
+% stimulus set into the directory structure expected by the MF3D_StimEditor
+% GUI. See https://github.com/MonkeyGone2Heaven/MF3D-Tools/wiki/MF3D-Stimulus-Editor
+% for further details.
 %
 % Written by murphyap@nih.gov
 %==========================================================================
 
+if ~exist('MF3D_root','var') || ~isfolder(MF3D_root)
+    MF3D_root           = uigetdir(cd, 'Select MF3D_R1 download directory');
+end
+MF3D_ExpDir         = fullfile(MF3D_root, 'MF3D_Expressions');
+MF3D_ExpDirRGB      = fullfile(MF3D_ExpDir, 'ColorImages');
+MF3D_ExpDirLabel    = fullfile(MF3D_ExpDir, 'LabelMaps');
+MF3D_IdDir          = fullfile(MF3D_root, 'MF3D_Identities');
+MF3D_IdDirRGB       = fullfile(MF3D_IdDir, 'ColorImages');
+MF3D_IdDirLabel     = fullfile(MF3D_IdDir, 'LabelMaps');
 
-Params.RootDir          = uigetdir(cd, 'Select directory containing downloaded MF3D R1 .zip files');
+IdentityFiles       = {'SD-4','SD-3','SD-2','SD-1','SD0','SD1','SD2','SD3','SD4','Labels'};
+ExpressionFiles     = {'Coo','Fear','Neutral','Threat','Tongue','Yawn','Labels'};
 
-Params.ExpDir           = fullfile(Params.RootDir, 'MF3D_Expressions');
-Params.ExpSummaryFile   = fullfile(Params.ExpDir, 'MF3D_ExpressionSet_Summary.csv');
-Params.IdDir            = fullfile(Params.RootDir, 'MF3D_Identities');
-Params.IdSummaryFile    = fullfile(Params.IdDir, 'MF3D_IdentitySet_Summary.csv');
+%========= Move expression files
+if ~isfolder(MF3D_ExpDirRGB)
+    mkdir(MF3D_ExpDirRGB);
+end
+for exp = 1:numel(ExpressionFiles)-1
+    SourceDir = fullfile(MF3D_ExpDir, ExpressionFiles{exp});
+    if ~isfolder(SourceDir)
+        error('MF3D directory ''%s'' was not found!\nComplete download or update MF3D root directory.', SourceDir);
+    end
+    movefile(SourceDir, MF3D_ExpDirRGB)
+end
+if ~isfolder(MF3D_ExpDirLabel)
+    mkdir(MF3D_ExpDirLabel);
+end
+SourceDir = fullfile(MF3D_ExpDir, ExpressionFiles{exp+1});
+movefile(SourceDir, MF3D_ExpDirLabel);
+
+%========= Move identity files
+if ~isfolder(MF3D_IdDirRGB)
+    mkdir(MF3D_IdDirRGB);
+end
+for id = 1:numel(IdentityFiles)-1
+    SourceDir = fullfile(MF3D_IdDir, IdentityFiles{id});
+    if ~isfolder(SourceDir)
+        error('MF3D directory ''%s'' was not found!', SourceDir);
+    end
+    movefile(SourceDir, MF3D_IdDirRGB)
+end
+if ~isfolder(MF3D_IdDirLabel)
+    mkdir(MF3D_IdDirLabel);
+end
+SourceDir = fullfile(MF3D_IdDir, IdentityFiles{id+1});
+movefile(SourceDir, MF3D_IdDirLabel);
